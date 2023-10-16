110170d8dfc83f560fce44b9df3d281eba43fa88
StijnTassenoy <stijn.tassenoy@gmail.com>
2023-10-16 14:22:21 +0200

    Added CSS and reindexed files

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,11 @@
 "use strict";
 
-const categories = ["media", "media_pubg", "media_gps"]
+const categories = ["media", "media_online", "media_pubg", "media_gps"]
 
 document.addEventListener("DOMContentLoaded", function() {
     createNavigation(categories);
+    addParallaxEffect();
+    
 });
 
 /** 
@@ -19,18 +21,46 @@ function createNavigation(navTitles){
         let navItem = document.createElement("a");
         navItem.href = "#";
         let icon = document.createElement("i");
-        icon.textContent = category;
+        let categoryDisplayName = category;
+        switch(category) {
+            case "media":
+                categoryDisplayName = "Original Gangster";
+                icon.classList.add("fa-solid");
+                icon.classList.add("fa-skull");
+                break;
+            case "media_online":
+                categoryDisplayName = "Online Gangster"
+                icon.classList.add("fa-solid");
+                icon.classList.add("fa-globe");
+                break;
+            case "media_pubg":
+                categoryDisplayName = "PUBG"
+                icon.classList.add("fa-solid");
+                icon.classList.add("fa-person-rifle");
+                break;
+            case "media_gps":
+                categoryDisplayName = "GPS"
+                icon.classList.add("fa-solid");
+                icon.classList.add("fa-map");
+              break;
+            default:
+                categoryDisplayName
+        }
+
+        navItem.appendChild(icon);
+        navItem.appendChild(document.createTextNode(categoryDisplayName));
+
         
         navItem.addEventListener("click", function() {
             loadSounds(category);
         });
         
-        navContainer.appendChild(navListItem);
         navListItem.appendChild(navItem);
-        navItem.appendChild(icon);
+        navContainer.appendChild(navListItem);
     });
     
 }
+
 
 
 /** 
@@ -62,6 +92,26 @@ function loadSounds(category) {
         .catch(error => {
             console.error("Error loading files:", error);
         });
+  
+    
+    const parallaxBg = document.querySelector(".parallax-bg");
+    switch(category) {
+        case "media":
+            parallaxBg.style.backgroundImage = 'url("/images/fap.png")';  
+            break;
+        case "media_online":
+            parallaxBg.style.backgroundImage = 'url("/images/online.jpg")';  
+            break;
+        case "media_pubg":
+            parallaxBg.style.backgroundImage = 'url("/images/gps.jpg")';  
+            break;
+        case "media_gps":
+            parallaxBg.style.backgroundImage = 'url("/images/gps.jpg")';  
+            break;
+        default:
+            parallaxBg.style.backgroundImage = 'url("/images/fap.png")'; 
+    }
+
 }
 
 /** 
@@ -73,4 +123,16 @@ function playSound(category, sound) {
     let trigger = new Audio("assets/" + category + "/" + sound);
     trigger.play();
     trigger.currentTime=0;
+}
+
+function addParallaxEffect(){
+    const parallaxBg = document.querySelector(".parallax-bg");
+
+    document.addEventListener("mousemove", function(event) {
+        const mouseX = (event.clientX - window.innerWidth / 2) / window.innerWidth;
+        const mouseY = (event.clientY - window.innerHeight / 2) / window.innerHeight;
+        const offsetX = mouseX * 30;
+        const offsetY = mouseY * 30;
+        parallaxBg.style.backgroundPosition = `${50 + offsetX}% ${50 + offsetY}%`;
+    });
 }
